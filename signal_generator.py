38d5b6a12bece97d065704e8798435e5c65fb50f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-13 01:16:51 +0200

    add frequency dependency

--- signal_generator.py
+++ signal_generator.py
@@ -5,10 +5,17 @@ import matplotlib.pyplot as plt
 
 def generate_signal():
     # Create a random signal
-    x = np.linspace(0, 1, 2000)  # TODO make range to be time related
-    y = np.random.random((len(x), 1)) + 3
-    z = np.sin(28 * x) + 1  # TODO set 28Hz frequency
-    y[400] = 1  # TODO set y = 1 at certain points according to z
+    x = np.linspace(0, 1, 2000)
+    
+    z = np.sin(28 * x) + 1  # set 28Hz frequency
+
+    # Conditionally set the y values
+    for n in range(len(x)):
+        if 1.5 < (np.sin(28 * n) + 1)/2000 < 1.6:
+            y[n] = 1  # TODO set y = 1 at certain points according to z
+        else: 
+            y = np.random.random((len(x), 1)) + 3
+
     return x, y, z
 
 
