94030038a451cea6e734ac3aee85dcaf3b91df5c
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-13 22:13:43 +0200

    create peaks in NMR signal

--- signal_generator.py
+++ signal_generator.py
@@ -1,32 +1,38 @@
 from math import *
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
-
+# TODO input peak point from outside of function
+# TODO make double and triple peaks
+# TODO make more realistic peak
 def generate_signal():
     # Create a random signal
-    x = np.linspace(0, 1, 2000)
+    data_points = np.linspace(0, 0.1, 2000) # generate data points
     
-    z = np.sin(28 * x) + 1  # set 28Hz frequency
+    LF_signal = np.sin(2 * pi * 28 * data_points) + 1  # set 28Hz frequency
 
     # Conditionally set the y values
-    for n in range(len(x)):
-        if 1.5 < (np.sin(28 * n) + 1)/2000 < 1.6:
-            y[n] = 1  # TODO set y = 1 at certain points according to z
+    HF_signal = np.zeros(len(data_points))
+    for n in range(len(data_points)):
+        if .495 < LF_signal[n] < .505:
+            HF_signal[n] = 1  # Set resonance
         else: 
-            y = np.random.random((len(x), 1)) + 3
+            HF_signal[n] = random.random() + 3 # set random values
 
-    return x, y, z
-
+    return data_points, HF_signal, LF_signal
 
 def plot(time, signal, sine_wave):
     # Plot the Signals
     plt.plot(time, signal)
     plt.plot(time, sine_wave)
     plt.ylim((0, 5))
+    plt.xlim((0, 0.1))
     plt.show()
 
 
 if __name__ == '__main__':
     t, b, n = generate_signal()
     plot(t, b, n)
+
+    print("Troubleshooting step")
