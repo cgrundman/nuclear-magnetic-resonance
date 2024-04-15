28d9b311818a046f6f3c4bf40258a94a8ce4e82e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-15 15:38:33 +0200

    add sweep of resonaces

--- signal_generator.py
+++ signal_generator.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import random
 
 
-# TODO connect resonance_frq to plot
+# TODO connect resonance_frq to HF_setting
 # TODO rename vaiables for clarity
 # TODO make save signal as data
 # TODO create searies of data through 16-20 MHz.
@@ -40,7 +40,7 @@ def generate_signal(resonance_frq):
     return data_points, HF_signal, LF_signal
 
 
-def plot(time, NMR_signal, LF_signal, HF_setting):
+def plot(time, NMR_signal, LF_signal, HF_setting, iteration):
     # Plot the Signals
     plt.style.use('dark_background')
     fig, (ax1, ax2) = plt.subplots(2, 1)
@@ -56,20 +56,22 @@ def plot(time, NMR_signal, LF_signal, HF_setting):
     ax1.set_ylim((0.5, 4.1))
     ax2.plot(time, LF_signal, color='deepskyblue')
     ax2.set_title("LF Signal")
-    # ax2.set_ylim((0, 5))
     ax2.set_xlim((0, 0.1))
     ax2.grid(color='dimgrey')
     ax2.set_xticklabels([])
     ax2.set_yticklabels([])
-    plt.savefig("figures/sample_signal.png")
+    plt.savefig(f"resonance_sweep/sample_signal_{iteration}.png")
 
 
 if __name__ == '__main__':
-    resonance_frq = .6
 
-    t, b, n = generate_signal(resonance_frq)
+    for i in range(20):
 
-    HF_setting = 18.5
-    plot(t, b, n, HF_setting)
+        resonance_frq = i/10
+
+        t, b, n = generate_signal(resonance_frq)
+
+        HF_setting = 18 + i/20
+        plot(t, b, n, HF_setting, i)
 
     print("Troubleshooting step")
