084e8e275c47c27e4d9c9db3ab52384d88c0129a
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-15 15:13:19 +0200

    add todo list, rename some variables

--- signal_generator.py
+++ signal_generator.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import random
 
 
+# TODO connect resonance_frq to plot
+# TODO rename vaiables for clarity
+# TODO make save signal as data
+# TODO create searies of data through 16-20 MHz.
 def generate_signal(resonance_frq):
     # Create a random signal
     data_points = np.linspace(0, 0.1, 2000) # generate data points
@@ -36,21 +40,21 @@ def generate_signal(resonance_frq):
     return data_points, HF_signal, LF_signal
 
 
-def plot(time, signal, sine_wave, HF_setting):
+def plot(time, NMR_signal, LF_signal, HF_setting):
     # Plot the Signals
     plt.style.use('dark_background')
     fig, (ax1, ax2) = plt.subplots(2, 1)
 
     fig.suptitle(f"{HF_setting} MHz")
 
-    ax1.plot(time, signal, color='lightcoral')
+    ax1.plot(time, NMR_signal, color='lightcoral')
     ax1.set_title("NMR Signal")
     ax1.set_xlim((0, 0.1))
     ax1.grid(color='dimgrey')
     ax1.set_xticklabels([])
     ax1.set_yticklabels([])
     ax1.set_ylim((0.5, 4.1))
-    ax2.plot(time, sine_wave, color='deepskyblue')
+    ax2.plot(time, LF_signal, color='deepskyblue')
     ax2.set_title("LF Signal")
     # ax2.set_ylim((0, 5))
     ax2.set_xlim((0, 0.1))
