1d22e84cb6de5c144f41bf9dabf96f51614525f6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-19 14:30:49 +0200

    create highlighted regions of usable data

--- signal_generator.py
+++ signal_generator.py
@@ -16,9 +16,9 @@ def generate_signal(material, HF_actual):
     peaks = material["Peaks"]
 
     # Create a random signal
-    data_points = np.linspace(0, 6/28, 2000) # generate data points
+    data_points = np.linspace(0, 5/28, 2000) # generate data points
     
-    LF_signal = (np.sin(2 * pi * 28 * data_points) + 1)*.2  # set 28Hz frequency
+    LF_signal = (np.sin(2 * pi * 28 * data_points - pi/2) + 1)*.2  # set 28Hz frequency
 
     # Create Baseline Signal
     HF_signal = np.random.rand(len(data_points)) + 3
@@ -50,15 +50,15 @@ def generate_signal(material, HF_actual):
                     HF_signal[n] = random.random()/10 + 2.5 + 40*abs((resonance_low-HF_actual)-(LF_signal[n]))
                 elif resonance - .02 < LF_signal[n] + HF_actual < resonance + .02:
                     HF_signal[n] = random.random()/10 + 1.5 + 50*abs((resonance-HF_actual)-(LF_signal[n]))
-        signal_data = {
-            'name': material['Name'],
-            'hf_setting': HF_actual,
-            'hf_signal': HF_signal,
-            'lf_signal': LF_signal
-        }
-        save_file = open("data/json_data.json", "w")
-        json.dump(signal_data, save_file)  
-        save_file.close()
+        # signal_data = {
+        #     'name': material['Name'],
+        #     'hf_setting': HF_actual,
+        #     'hf_signal': HF_signal,
+        #     'lf_signal': LF_signal
+        # }
+        # save_file = open("data/json_data.json", "w")
+        # json.dump(signal_data, save_file)  
+        # save_file.close()
 
     return data_points, HF_signal, LF_signal
 
@@ -76,7 +76,7 @@ def sweep(material):
         t, b, n = generate_signal(material, HF_actual)
 
         # Plot 
-        # plot(t, b, n, HF_setting, HF_actual, iteration)
+        plot(t, b, n, HF_setting, HF_actual, iteration)
 
         HF_setting += .03125
         iteration += 1
@@ -84,25 +84,45 @@ def sweep(material):
 
 
 def plot(time, NMR_signal, LF_signal, HF_setting, HF_actual, iteration):
+    
     # Plot the Signals
     plt.style.use('dark_background')
     fig, (ax1, ax2) = plt.subplots(2, 1)
-
     fig.suptitle(f"Setting: {HF_setting:.4f} MHz      Actual: {round(HF_actual, 4)} MHz")
 
+    # Subplot 1
     ax1.plot(time, NMR_signal, color='lightcoral')
     ax1.set_title("NMR Signal")
-    ax1.set_xlim((0, 6/28))
     ax1.grid(color='dimgrey')
     ax1.set_xticklabels([])
     ax1.set_yticklabels([])
+    
+    ax1.set_xlim((0, 5/28))
     ax1.set_ylim((0.5, 4.1))
+
+    # Subplot 2
     ax2.plot(time, LF_signal, color='deepskyblue')
     ax2.set_title("LF Signal")
-    ax2.set_xlim((0, 6/28))
     ax2.grid(color='dimgrey')
     ax2.set_xticklabels([])
     ax2.set_yticklabels([])
+    ax2.set_xlim((0, 5/28))
+
+    # Create highlighted ranges
+    range_points = np.zeros([2,6])
+    i, j = 0, 0
+    for n in range(len(LF_signal)):
+        if LF_signal[n-1]<=.1 and LF_signal[n]>=.1:
+            range_points[0,i] = time[n]
+            i += 1
+        if LF_signal[n-1]<=.3 and LF_signal[n]>=.3:
+            range_points[1,j] = time[n]
+            j += 1
+    for i in range(len(range_points[0,:])):
+        ax1.axvspan(range_points[0,i], range_points[1,i], color='yellow', alpha=0.5)
+        ax2.axvspan(range_points[0,i], range_points[1,i], color='yellow', alpha=0.5)
+
+    # Save and close plot
     plt.savefig(f"full_sweep/material_sample_{iteration}.png")
     plt.close()
 
@@ -132,5 +152,14 @@ if __name__ == '__main__':
         'Resonances': [16.8, 18.9],
         'Peaks': [1, 3]
     }
+    material = {
+        'Name': "Material 5",
+        'Resonances': [16.2, 17.5],
+        'Peaks': [1, 1]
+    }
 
     sweep(material)
+
+    print("Sweep")
+    print("Stop")
+    print("End")
