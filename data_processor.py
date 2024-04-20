edde44fbe9242d5ce64fe282d17b6db4d20c6658
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-20 10:54:28 +0200

    iterate through files within directory

--- data_processor.py
+++ data_processor.py
@@ -1,12 +1,31 @@
 import numpy as np
+import os
+
 
 # TODO Create main funciton
 # TODO load all data
 # TODO Combine all samples from the same HF setting
 # TODO Combine two nmr samples with different HF settings
 # TODO Create single nmr resonance data 
-path = 'data/unprocessed_nmr_data/Material_1/'
 
-nmr = np.loadtxt(path + "159938_nmr.txt")
+if __name__ == '__main__':
+    path = 'data/unprocessed_nmr_data/Material_1/'
+    directory = os.fsencode(path)
 
-print(len(nmr))
+    # Iterate through NMR files
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith("nmr.txt"):
+            # print(os.path.join(directory, filename))
+            print(filename)
+            continue
+
+    # Iterate through LF files
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith("lf.txt"):
+            # print(os.path.join(directory, filename))
+            print(filename)
+            continue
+
+    # nmr = np.loadtxt(path + "159938_nmr.txt")
