7b22f3a7ae00546bd77d8d4f302f79066a422458
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-21 12:57:44 +0200

    plot merged iteration process

--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import os
 
 
@@ -59,8 +61,135 @@ def merge_iteration(NMR_signal, LF_signal):
     LF_merged = LF_merged/5
 
     # Plot Slices
+    plot_merge_iteration(NMR_signal, NMR_slices, NMR_merged, LF_signal, LF_slices, LF_merged)
     print(np.shape(NMR_merged))
     print(np.shape(LF_merged))
+
+
+def plot_merge_iteration(NMR_signal, NMR_slices, NMR_merged, LF_signal, LF_slices, LF_merged):
+    
+    # Define number of slices
+    slices = np.shape(NMR_slices)[0]
+
+    # Plot the Signals
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(10, 10))
+    gs = GridSpec(nrows=6, ncols=slices)
+    # fig, (ax0, ax1, ax2, ax3, ax4, ax5) = plt.subplots(6, 5)
+    fig.suptitle("Merge Iteration")
+
+    # Orignal Data
+    ax0 = fig.add_subplot(gs[0,:])
+    ax0.plot(NMR_signal, color='lightcoral') # NMR signal
+    ax0.set_title("Original Data")
+    ax0.grid(color='dimgrey')
+    ax0.set_xticklabels([])
+    ax0.set_yticklabels([])
+    ax0.set_ylim((.9, 4.1))
+    ax1 = fig.add_subplot(gs[1,:])
+    ax1.plot(LF_signal, color='deepskyblue') # LF signal
+    ax1.grid(color='dimgrey')
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+
+    
+    # Data Slices
+    ax4 = fig.add_subplot(gs[2,0])
+    ax4.plot(NMR_slices[0,:], color='lightcoral')
+    ax4.grid(color='dimgrey')
+    ax4.set_xticklabels([])
+    ax4.set_yticklabels([])
+    ax4.set_ylim((.9, 4.1))
+    ax5 = fig.add_subplot(gs[2,1])
+    ax5.plot(NMR_slices[1,:], color='lightcoral')
+    ax5.grid(color='dimgrey')
+    ax5.set_xticklabels([])
+    ax5.set_yticklabels([])
+    ax5.set_ylim((.9, 4.1))
+    ax6 = fig.add_subplot(gs[2,2])
+    ax6.plot(NMR_slices[2,:], color='lightcoral')
+    ax6.grid(color='dimgrey')
+    ax6.set_xticklabels([])
+    ax6.set_yticklabels([])
+    ax6.set_ylim((.9, 4.1))
+    ax7 = fig.add_subplot(gs[2,3])
+    ax7.plot(NMR_slices[3,:], color='lightcoral')
+    ax7.grid(color='dimgrey')
+    ax7.set_xticklabels([])
+    ax7.set_yticklabels([])
+    ax7.set_ylim((.9, 4.1))
+    ax8 = fig.add_subplot(gs[2,4])
+    ax8.plot(NMR_slices[4,:], color='lightcoral')
+    ax8.grid(color='dimgrey')
+    ax8.set_xticklabels([])
+    ax8.set_yticklabels([])
+    ax8.set_ylim((.9, 4.1))
+    ax6.set_title("Sliced Data")
+
+    ax9 = fig.add_subplot(gs[3,0])
+    ax9.plot(LF_slices[0,:], color='deepskyblue')
+    ax9.grid(color='dimgrey')
+    ax9.set_xticklabels([])
+    ax9.set_yticklabels([])
+    ax10 = fig.add_subplot(gs[3,1])
+    ax10.plot(LF_slices[1,:], color='deepskyblue')
+    ax10.grid(color='dimgrey')
+    ax10.set_xticklabels([])
+    ax10.set_yticklabels([])
+    ax11 = fig.add_subplot(gs[3,2])
+    ax11.plot(LF_slices[2,:], color='deepskyblue')
+    ax11.grid(color='dimgrey')
+    ax11.set_xticklabels([])
+    ax11.set_yticklabels([])
+    ax12 = fig.add_subplot(gs[3,3])
+    ax12.plot(LF_slices[3,:], color='deepskyblue')
+    ax12.grid(color='dimgrey')
+    ax12.set_xticklabels([])
+    ax12.set_yticklabels([])
+    ax13 = fig.add_subplot(gs[3,4])
+    ax13.plot(LF_slices[4,:], color='deepskyblue')
+    ax13.grid(color='dimgrey')
+    ax13.set_xticklabels([])
+    ax13.set_yticklabels([])
+    # for slice in range(slices):
+    #     ax[slice]
+    # x0 = 0
+    # for slice in range(slices):
+    #     num = slice + 2
+    #     x{num} = fig.add_subplot(gs[2,slice])
+        
+
+    # ax2.plot(time, LF_signal, color='deepskyblue')
+    # ax2.set_title("LF Signal")
+    # ax2.grid(color='dimgrey')
+    # ax2.set_xticklabels([])
+    # ax2.set_yticklabels([])
+    # ax2.set_xlim((0, 5/28))
+
+    # Merged Data
+    ax2 = fig.add_subplot(gs[4,1:-1])
+    ax2.plot(NMR_merged, color='lightcoral') # NMR merged
+    ax2.set_title("Merged Data")
+    ax2.grid(color='dimgrey')
+    ax2.set_xticklabels([])
+    ax2.set_yticklabels([])
+    ax2.set_ylim((.9, 4.1))
+    ax3 = fig.add_subplot(gs[5,1:-1])
+    ax3.plot(LF_merged, color='deepskyblue') # LF merged
+    ax3.grid(color='dimgrey')
+    ax3.set_xticklabels([])
+    ax3.set_yticklabels([])
+
+    # ax2.plot(time, LF_signal, color='deepskyblue')
+    # ax2.set_title("LF Signal")
+    # ax2.grid(color='dimgrey')
+    # ax2.set_xticklabels([])
+    # ax2.set_yticklabels([])
+    # ax2.set_xlim((0, 5/28))
+
+    # Save and close plot
+    plt.savefig(f"figures/merge_iteration.png")
+    plt.close()
 
 
 if __name__ == '__main__':
