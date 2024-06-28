eb7f2158088d0ff7681289794e5fac32cc6d3ef5
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-28 23:23:30 +0200

    add updating functionality to app from button click

--- nmr-app/app.py
+++ nmr-app/app.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import numpy as np
+import random
 
 from nmr_signal import nmr_signal_generator
 from plot import plot, bar
@@ -46,20 +47,38 @@ materials = [
 # NMR function
 def nmr_function():
 
-    # Plot NMR Signal
-    x, lf_signal, nmr_signal = nmr_signal_generator(material=materials[4], HF_actual=16.0000)
-    fig = plot(name="NMR Signal", x=x, y=[lf_signal*2-0.5, nmr_signal], plot_rgb=["#4976fc", "#ff4f4d"])
-    canvas = FigureCanvasTkAgg(fig, master=frame3)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=0, column=0)
+    HF_setting = 15.75
 
-    # Plot NMR Spectrum
-    x = np.linspace(16, 20, 1200)
-    nmr_spectrum = np.random.rand(len(x))
-    fig2 = plot(name="NMR Spectrum", x=x, y=[nmr_spectrum], plot_rgb=["#02edaf"])
-    canvas = FigureCanvasTkAgg(fig2, master=frame4)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=0, column=0)
+    # while HF_setting <= 20:
+    for i in range(5):
+
+        # Add Randomization of the HF Setting
+        HF_actual = HF_setting + ((random.random()-.5)/50)
+
+        # Generate NMR Signal
+        x, lf_signal, nmr_signal = nmr_signal_generator(material=materials[4], HF_actual=HF_actual)
+        
+        # Plot NMR Signal
+        fig = plot(name="NMR Signal", x=x, y=[lf_signal*2-0.5, nmr_signal], plot_rgb=["#4976fc", "#ff4f4d"])
+        canvas = FigureCanvasTkAgg(fig, master=frame3)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=0, column=0)
+
+        HF_setting += .03125
+
+        root.after(500)
+
+    
+
+        # # Plot NMR Spectrum
+        # x = np.linspace(16, 20, 1200)
+        # nmr_spectrum = np.random.rand(len(x))
+        # fig2 = plot(name="NMR Spectrum", x=x, y=[nmr_spectrum], plot_rgb=["#02edaf"])
+        # canvas = FigureCanvasTkAgg(fig2, master=frame4)
+        # canvas.draw()
+        # canvas.get_tk_widget().grid(row=0, column=0)
+
+        
 
 
 # Tkinter App
@@ -134,124 +153,3 @@ close_button.grid(ipady=10, ipadx=10, pady=12)
 label8 = tk.Label(root, text="Material Selection", wraplength=1, font=('Helvetica', font_size), bg=bg_color, highlightbackground="black", highlightthickness=2, width=30, height=40)
 label8.grid(row=0, rowspan=4, column=5)
 
-# # Create the main Tkinter window
-# root = tk.Tk()
-# root.title("NMR Application")
-# root.geometry("1200x525")
-# root.configure(bg='#4c4c4c')
-
-# # Create a style for the close button
-# style = ttk.Style()
-# style.configure('Red.TButton', 
-#                 background='#dc3545', 
-#                 foreground='#dc3545', 
-#                 font=('Helvetica', 12),
-#                 padding=6)
-# style.map('Red.TButton',
-#           background=[('active', '#dc3545'), ('pressed', '#bd2130')])
-
-# # Create a style for the plot button
-# style = ttk.Style()
-# style.configure('Blue.TButton', 
-#                 background='#3556dc', 
-#                 foreground='#3556dc', 
-#                 font=('Helvetica', 12),
-#                 padding=6)
-# style.map('Red.TButton',
-#           background=[('active', '#3556dc'), ('pressed', '#bd2130')])
-
-# # Create a frame for the plot
-# plot_frame = tk.Frame(root)
-# plot_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-# # Create a label
-# label = tk.Label(root, text="NMR Application", bg='#4c4c4c')
-# label.pack(pady=20)
-
-# # # Function to create a plot
-# # def create_plots():
-# #     fig = Figure(figsize=(10, 4), dpi=100, constrained_layout=True)
-# #     # fig.tight_layout()
-    
-# #     # Create subplot 1
-# #     ax1 = fig.add_subplot(211)
-# #     x1 = np.linspace(0, 200, 100)
-# #     y1 = np.random.rand(100)
-# #     y1_sin = np.sin(x1) + 0.5
-# #     ax1.plot(x1, y1, y1_sin)
-# #     ax1.set_title("NMR Signal") # TODO name better
-# #     # ax1.set_yticks([])
-# #     ax1.set_xlim([0, 200])
-# #     ax1.set_ylim([0, 4])
-# #     ax1.grid()
-    
-# #     # Create subplot 2
-# #     range = [16, 20]
-# #     length = 1200
-# #     ax2 = fig.add_subplot(212)
-# #     x2 = np.linspace(range[0], range[1], length)
-# #     y2 = np.random.rand(length)*0.5
-# #     ax2.plot(x2, y2)
-# #     ax2.set_title("NMR Data") # TODO name better
-# #     # ax2.set_yticks([])
-# #     ax2.set_xlim([range[0], range[1]])
-# #     ax2.set_ylim([0, 4])
-# #     ax2.grid()
-
-# #     canvas = FigureCanvasTkAgg(fig, master=root)  # A Tkinter canvas for the Matplotlib figure
-# #     canvas.draw()
-# #     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-# def create_plots():
-#     # y1 = np.random.rand((len(x1)))
-#     # ax1.plot(x1, y1, y1_sin)
-#     pass
-
-# # fig = Figure(figsize=(10, 4), dpi=100, constrained_layout=True)
-# # fig.patch.set_facecolor('#4c4c4c')
-
-# # # Create subplot 1
-# # ax1 = fig.add_subplot(211)
-# # x1 = np.linspace(0, 200, 100)
-# # y1 = np.zeros((len(x1)))
-# # y1_sin = np.zeros((len(x1)))
-# # ax1.plot(x1, y1, y1_sin)
-# # ax1.set_title("NMR Signal", color=(0.9, 0.9, 0.9)) # TODO name better
-# # # ax1.set_yticks([])
-# # ax1.set_xlim([0, 200])
-# # ax1.set_ylim([0, 4])
-# # ax1.set_facecolor((0.5, 0.5, 0.5))
-# # ax1.grid()
-
-# # # Create subplot 2
-# # range = [16, 20]
-# # length = 1200
-# # ax2 = fig.add_subplot(212)
-# # x2 = np.linspace(range[0], range[1], length)
-# # y2 = np.zeros((len(x2)))
-# # ax2.plot(x2, y2)
-# # ax2.set_title("NMR Data", color=(0.9, 0.9, 0.9)) # TODO name better
-# # # ax2.set_yticks([])
-# # ax2.set_xlim([range[0], range[1]])
-# # ax2.set_ylim([0, 4])
-# # ax2.set_facecolor((0.5, 0.5, 0.5))
-# # ax2.grid()
-
-# # canvas = FigureCanvasTkAgg(fig, master=root)  # A Tkinter canvas for the Matplotlib figure
-# # canvas.draw()
-# # canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-# # Create a frame for HF Setting
-# hf_signal_frame = tk.Frame(root, bg='#4c4c4c', highlightbackground="black", highlightthickness=2)
-# hf_signal_frame.pack(side=tk.TOP, fill=tk.X, pady=10)
-# label = tk.Label(root, text="NMR Application", bg='#4c4c4c')
-# label.pack(pady=20)
-
-# # Create a frame for the buttons
-# button_frame = tk.Frame(root, bg='#4c4c4c', highlightbackground="black", highlightthickness=1)
-# button_frame.pack(side=tk.BOTTOM, fill=tk.X, pady=10)
-
-# # Button to create the plots
-# plot_button = ttk.Button(button_frame, text="Plot Data", command=create_plots(), style='Blue.TButton')
-# plot_button.pack(side=tk.LEFT, padx=10)
-
-root.mainloop()
