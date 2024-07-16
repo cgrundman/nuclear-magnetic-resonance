905252aea9efa9747477fbe7b166f648439cdfff
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-16 21:52:06 +0200

    load model using weights

--- nmr-app/sandbox.py
+++ nmr-app/sandbox.py
@@ -1,6 +1,37 @@
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ['TF_KERAS'] = '1'
+
 import tensorflow as tf
+import keras
+import numpy as np
 
-from tensorflow import keras
+print(keras.__version__)
 
-model_dir = "models\model_nn.keras"
-model = keras.models.load_model(model_dir)
+model_weights_dir = 'models\model_nn_weights.h5'
+
+print(model_weights_dir)
+
+# Create the model
+model_nn = keras.models.Sequential([
+    keras.layers.InputLayer((1200,)),
+    keras.layers.Dense(8, activation="relu"),
+    keras.layers.Dense(8, activation="relu"),
+    keras.layers.Dense(5, activation="softmax"),
+])
+
+# 2. Compile the model
+model_nn.compile(loss=keras.losses.SparseCategoricalCrossentropy(),
+                 optimizer=keras.optimizers.Adam(),
+                 metrics=["accuracy"])
+
+model_nn.load_weights(model_weights_dir)
+
+input = tf.expand_dims(tf.zeros(1200), axis=0)
+print(input)
+
+pred = model_nn.predict(input)
+
+print(pred[0])
